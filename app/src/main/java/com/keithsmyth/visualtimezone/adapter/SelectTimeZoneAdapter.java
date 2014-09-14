6b6a022c195970784c33281b432e85e47ba843a8
Keith Smyth <keith.smyth@outware.com.au>
2014-09-14 14:50:55 +1000

    Filter timezone using contain logic

--- app/src/main/java/com/keithsmyth/visualtimezone/adapter/SelectTimeZoneAdapter.java
+++ app/src/main/java/com/keithsmyth/visualtimezone/adapter/SelectTimeZoneAdapter.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
+import android.widget.Filter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -20,10 +22,26 @@ public class SelectTimeZoneAdapter extends ArrayAdapter<String> {
 
     private final Set<String> mSelectedTimeZones;
 
+    private List<String> mObjects;
+    private List<String> mOriginalValues;
+    private final Object mLock = new Object();
+    private Filter mFilter;
+
     public SelectTimeZoneAdapter(Context context, int resource, int textViewResourceId,
                                  String[] objects) {
         super(context, resource, textViewResourceId, objects);
         mSelectedTimeZones = new HashSet<String>();
+        mObjects = new ArrayList<String>(Arrays.asList(objects));
+    }
+
+    @Override
+    public String getItem(int position) {
+        return mObjects.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mObjects.size();
     }
 
     @Override
@@ -66,5 +84,80 @@ public class SelectTimeZoneAdapter extends ArrayAdapter<String> {
     private boolean isSelected(int position) {
         String timeZone = getItem(position);
         return mSelectedTimeZones.contains(timeZone);
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (mFilter == null) {
+            mFilter = new ArrayFilter();
+        }
+        return mFilter;
+    }
+
+    private class ArrayFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence prefix) {
+            FilterResults results = new FilterResults();
+
+            if (mOriginalValues == null) {
+                synchronized (mLock) {
+                    mOriginalValues = new ArrayList<String>(mObjects);
+                }
+            }
+
+            if (prefix == null || prefix.length() == 0) {
+                ArrayList<String> list;
+                synchronized (mLock) {
+                    list = new ArrayList<String>(mOriginalValues);
+                }
+                results.values = list;
+                results.count = list.size();
+            } else {
+                String prefixString = prefix.toString().toLowerCase();
+
+                ArrayList<String> values;
+                synchronized (mLock) {
+                    values = new ArrayList<String>(mOriginalValues);
+                }
+
+                final ArrayList<String> newValues = new ArrayList<String>();
+
+                for (final String value : values) {
+                    final String valueText = value.toLowerCase();
+
+                    // First match against the whole, non-splitted value
+                    if (valueText.contains(prefixString)) {
+                        newValues.add(value);
+                    }
+                    else {
+                        final String[] words = valueText.split(" ");
+
+                        // Start at index 0, in case valueText starts with space(s)
+                        for (String word : words) {
+                            if (word.contains(prefixString)) {
+                                newValues.add(value);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                results.values = newValues;
+                results.count = newValues.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            //noinspection unchecked
+            mObjects = (List<String>) results.values;
+            if (results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
     }
 }
